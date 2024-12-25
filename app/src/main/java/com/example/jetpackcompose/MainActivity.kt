b84ecf0162de4adbf21391c52e35120cc6d04964
Amitesh Singh <amiteshsinghk@gmail.com>
2024-12-25 17:00:20 +0530

    1. custom row layout 2. Sub compose layout

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -14,13 +14,20 @@ import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.example.jetpackcompose.masterclass.HotelBookingScreen
 import com.example.jetpackcompose.masterclass.SubComposePageRow
+import com.example.jetpackcompose.masterclass.measurement.LazyMindMap
+import com.example.jetpackcompose.masterclass.measurement.MindMapItem
 import com.example.jetpackcompose.masterclass.todoScreen.ToDoScreenRoot
 import com.example.jetpackcompose.masterclass.todoScreen.ToDoViewModel
 import kotlin.random.Random
@@ -32,34 +39,89 @@ class MainActivity : ComponentActivity() {
 //        Log.d("MainActivity","onCreate: ${viewModel.hashCode()}")
         enableEdgeToEdge()
         setContent {
-//                HotelBookingScreen()
-            val currentPage = remember {
-                mutableIntStateOf(0)
-            }
-            Column(modifier = Modifier
-                .fillMaxSize()
-                .safeDrawingPadding()) {
-                SubComposePageRow(
-                    page = currentPage.value,
-                    modifier = Modifier
-                ) {
-                    for (i in 0..1000) {
-                        Box(
-                            modifier = Modifier
-                                .width(Random.nextInt(300).dp)
-                                .height(100.dp)
-                                .background(Color(Random.nextInt()))
-                        )
-                    }
-                }
-                Button(
-                    onClick = { currentPage.value++ }
-                ) {
-                    Text(text = "Next")
-                }
-            }
-
 //            ToDoScreenRoot()
+//            val currentPage = remember {
+//                mutableIntStateOf(0)
+//            }
+//            Column(modifier = Modifier
+//                .fillMaxSize()
+//                .safeDrawingPadding()) {
+//                SubComposePageRow(
+//                    page = currentPage.value,
+//                    modifier = Modifier
+//                ) {
+//                    for (i in 0..1000) {
+//                        Box(
+//                            modifier = Modifier
+//                                .width(Random.nextInt(300).dp)
+//                                .height(100.dp)
+//                                .background(Color(Random.nextInt()))
+//                        )
+//                    }
+//                }
+//                Button(
+//                    onClick = { currentPage.value++ }
+//                ) {
+//                    Text(text = "Next")
+//                }
+//            }
+//
+        var mindMapItems = remember {
+            listOf(
+                MindMapItem(
+                    title = "HelloWorld 1",
+                    percentageOffset = Offset(
+                        x = 0f,
+                        y = 0f
+                    )
+                ),
+                MindMapItem(
+                    title = "HelloWorld 2",
+                    percentageOffset = Offset(
+                        x = 1f,
+                        y = -0.5f
+                    )
+                ),
+                MindMapItem(
+                    title = "HelloWorld 3",
+                    percentageOffset = Offset(
+                        x = 0.3f,
+                        y = -0.75f
+                    )
+                ),
+                MindMapItem(
+                    title = "HelloWorld 4",
+                    percentageOffset = Offset(
+                        x = .5f,
+                        y = .05f
+                    )
+                ),
+                MindMapItem(
+                    title = "HelloWorld 5",
+                    percentageOffset = Offset(
+                        x = .25f,
+                        y = .25f
+                    )
+                )
+
+
+            )
+        }
+            var mindMapOffSet by remember {
+                mutableStateOf(IntOffset.Zero)
+            }
+            LazyMindMap(
+                items = mindMapItems,
+                mindMapOffSet = mindMapOffSet,
+                onDrag = {delta ->
+                    mindMapOffSet += delta
+                },
+
+                modifier = Modifier
+                    .fillMaxSize()
+                    .safeDrawingPadding()
+            )
+
         }
     }
 }
