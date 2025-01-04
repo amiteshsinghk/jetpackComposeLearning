fafddf296131158d48e2a2bfde421e5a24f4b3e4
Amitesh Singh <amiteshsinghk@gmail.com>
2025-01-04 11:36:30 +0530

    Measurements

---
+++ app/src/main/java/com/example/jetpackcompose/masterclass/measurement/SimpleCustomModifier.kt
@@ -0,0 +1,41 @@
+package com.example.jetpackcompose.masterclass.measurement
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
+
+inline fun Modifier.applyIf(
+    condition: Boolean,
+    modifier: Modifier.() -> Modifier
+): Modifier {
+    return if(condition) {
+        this.then(modifier())
+    } else this
+}
+
+@Composable
+fun SimpleModifierDemo(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .size(100.dp)
+            .applyIf(true) {
+                background(Color.Red)
+                    .padding(16.dp)
+            }
+    )
+}
+
+@Preview
+@Composable
+private fun SimpleModifierDemoPreview() {
+    JetpackComposeTheme {
+        SimpleModifierDemo()
+    }
+}
